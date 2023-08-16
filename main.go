6f7888597e8510647aca8e94506e7668f02233ec
Utku Karagül <utkutr07@gmail.com>
2023-08-17 02:10:07 +0300

    Fonksiyonlar iyileştirildi

--- main.go
+++ main.go
@@ -18,28 +18,25 @@ func main() {
 
 	var convCmd = &cobra.Command{
 		Use:   "conv",
-		Short: "Enter the directory of the file to change the extension",
-		Args:  cobra.NoArgs,
+		Short: "Convert file format",
 		Run: func(cmd *cobra.Command, args []string) {
-			getFileName(cmd, args)
+			converter(cmd, args)
 		},
 	}
 
 	var outCmd = &cobra.Command{
 		Use:   "outDir",
-		Short: "Enter the directory to save the formatted file",
+		Short: "The directory of the converted file",
 		Run: func(cmd *cobra.Command, args []string) {
-			err := saveContentToDirectory(cmd, "a", "b")
-			if err != nil {
-				fmt.Println("Error saving file:", err)
-				return
-			}
+			content, _ := readContent(cmd, args)
+			newName := converter(cmd, args)
+			saveContentToDirectory(cmd, content, newName)
 		},
 	}
 
 	var formatCmd = &cobra.Command{
-		Use:   "Format",
-		Short: "Enter the extension to which the given file will be converted",
+		Use:   "format",
+		Short: "Format of the new file",
 		Run: func(cmd *cobra.Command, args []string) {
 			converter(cmd, args)
 		},
@@ -57,34 +54,26 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
 }
 
 func converter(cmd *cobra.Command, args []string) string {
-	//Girilen uzantı ve önceki uzantısından temizlenmiş dosya adı oluşturur
 	lastFileName := getFileName(cmd, args)
 	fileFormat, _ := cmd.Flags().GetString("format")
-
 	newName := lastFileName + fileFormat
 	return newName
 }
 
 func isValidDirectory(fileDir string) bool {
-	//Gelen dizinin geçerli olup olmadığını kontrol eder
 	fileInfo, err := os.Stat(fileDir)
 	if err != nil {
 		return false
 	}
-
 	return fileInfo.IsDir()
 }
 
 func getFileName(cmd *cobra.Command, args []string) string {
-	//Gelen dosyanın dizini geçerli ise sondaki uzantıyı ve baştaki dizini temizler
 	fileName, _ := cmd.Flags().GetString("file")
-
 	if isValidDirectory(fileName) == true {
-
 		parts := strings.Split(fileName, ".")
 		if len(parts) > 1 {
 			baseName := strings.Join(parts[:len(parts)-1], ".")
@@ -99,37 +88,31 @@ func getFileName(cmd *cobra.Command, args []string) string {
 }
 
 func readContent(cmd *cobra.Command, args []string) (string, error) {
-	//dosya içeriğini okur
 	filePath, _ := cmd.Flags().GetString("file")
-
 	content, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		return "", err
 	}
-
 	return string(content), nil
 }
 
-func saveContentToDirectory(cmd *cobra.Command, content string, newName string) error {
+func saveContentToDirectory(cmd *cobra.Command, content string, newName string) {
 	outDir, _ := cmd.Flags().GetString("dir")
-
 	if strings.HasSuffix(outDir, "/") {
 		newFilePath := filepath.Join(outDir, newName)
-
 		file, err := os.Create(newFilePath)
 		if err != nil {
-			return err
+			fmt.Println("Error creating file:", err)
+			return
 		}
 		defer file.Close()
-
 		_, err = file.WriteString(content)
 		if err != nil {
-			return err
+			fmt.Println("Error writing content to file:", err)
+			return
 		}
-
 		fmt.Println("File saved at:", newFilePath)
-		return nil
+	} else {
+		fmt.Println("Invalid directory")
 	}
-
-	return fmt.Errorf("Invalid directory")
 }
